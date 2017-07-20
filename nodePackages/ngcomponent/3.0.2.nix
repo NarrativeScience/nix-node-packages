ae6377faa570f7d0e5c4e2a54ea32996ff5d9b90
ns-ctruong <ctruong@narrativescience.com>
2017-07-20 16:40:18 -0500

    QPT-7773 Add react2angular to nix

---
+++ nodePackages/ngcomponent/3.0.2.nix
@@ -0,0 +1,32 @@
+{ buildNodePackage, namespaces, nodePackages, pkgs }:
+buildNodePackage {
+    name = "ngcomponent";
+    version = "3.0.2";
+    src = pkgs.fetchurl {
+      url = "https://registry.npmjs.org/ngcomponent/-/ngcomponent-3.0.2.tgz";
+      sha1 = "d67f89d892ec5806c6d1445c5f195a6ce6ec5486";
+    };
+    deps = with nodePackages; [
+      namespaces.types.lodash-mapvalues_4-6-2
+      lodash-some_4-6-0
+      lodash-assign_4-2-0
+      namespaces.types.lodash-assign_4-2-2
+      namespaces.types.lodash-some_4-6-2
+      lodash-mapvalues_4-6-0
+      angular_1-6-5
+      namespaces.types.angular_1-6-27
+    ];
+    meta = {
+      homepage = "https://github.com/coatue-oss/ngcomponent#readme";
+      description = "A clean React-like abstraction for rendering non-Angular components within an Angular app.";
+      keywords = [
+        "angular"
+        "component"
+        "react"
+        "directive"
+        "props"
+        "state"
+        "controller"
+      ];
+    };
+  }
