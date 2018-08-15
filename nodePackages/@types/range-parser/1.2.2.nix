c73b15a548f437c086118b96e8f810db6a4b82f7
Jonathan Drake <jdrake@narrativescience.com>
2018-08-14 20:00:58 -0700

    QUILLT-221 Adds jsonwebtoken

---
+++ nodePackages/@types/range-parser/1.2.2.nix
@@ -0,0 +1,16 @@
+{ buildNodePackage, fetchUrlWithHeaders, namespaceTokens, nodePackages, pkgs }:
+buildNodePackage {
+    name = "range-parser";
+    version = "1.2.2";
+    src = fetchUrlWithHeaders {
+      url = "https://registry.npmjs.org/@types/range-parser/-/range-parser-1.2.2.tgz";
+      sha1 = "fa8e1ad1d474688a757140c91de6dace6f4abc8d";
+    };
+    patchPhase = "touch index.js";
+    namespace = "types";
+    deps = [];
+    devDependencies = [];
+    meta = {
+      description = "TypeScript definitions for range-parser";
+    };
+  }
